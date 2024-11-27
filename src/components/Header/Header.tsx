92ff4d4b5f49cf0c3cc923750a02b2ef831cb3bd
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-27 19:09:58 +0100

    Create a header container and change h1 html tag to span

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,14 +3,16 @@ import "./Header.css";
 const Header: React.FC = () => {
   return (
     <header className="main-header">
-      <img
-        className="main-header__logo"
-        src="/images/logo.svg"
-        alt="Raceinator logo"
-        width={88}
-        height={50}
-      />
-      <h1 className="main-header__title">Raceinator</h1>
+      <div className="main-header__container">
+        <img
+          className="main-header__logo"
+          src="/images/logo.svg"
+          alt="Raceinator logo"
+          width={88}
+          height={50}
+        />
+        <span className="main-header__title">Raceinator</span>
+      </div>
     </header>
   );
 };
