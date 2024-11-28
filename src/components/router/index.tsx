66e22bc6f333c66b14fbb87b25cd21e47d5849ea
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-28 22:50:43 +0100

    Add path route for NotFoundPage, add redirects to index.html and robots.txt

--- src/components/router/index.tsx
+++ src/components/router/index.tsx
@@ -6,12 +6,14 @@ import {
 } from "react-router-dom";
 import App from "../App/App";
 import TeamsPage from "../../teams/pages/TeamsPage/TeamsPage";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Navigate to="/home" />} />
       <Route path="home" element={<TeamsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Route>,
   ),
 );
