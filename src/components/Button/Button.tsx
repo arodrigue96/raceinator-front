ee1084978588906bcb154a80c407c02f001900fb
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-28 22:43:42 +0100

    Create Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import "./Button.css";
+
+interface ButtonProps {
+  text: string;
+  className?: string;
+  redirection: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ text, className, redirection }) => {
+  return (
+    <Link className={className} to={redirection}>
+      {text}
+    </Link>
+  );
+};
+
+export default Button;
