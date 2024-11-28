7a68d291593fedcb5b90d8f7add1a89010e168fa
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-28 20:34:25 +0100

    Create NotFoundPage component

---
+++ src/components/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,23 @@
+import Button from "../../Button/Button";
+import "./NotFoundPage.css";
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <>
+      <h1 className="page-title">Page not found</h1>
+      <div className="container">
+        <p className="text">
+          This page doesn't exist. Not to worry, it happens to even the best
+          riders & teams.
+        </p>
+        <Button
+          text={"Go Back Home"}
+          redirection={"/home"}
+          className="button"
+        />
+      </div>
+    </>
+  );
+};
+
+export default NotFoundPage;
