a1b152d82178f00e917d9f0173cf6aaeb74642b6
Aniol Rodriguez <arodrigue96@gmail.com>
2024-12-05 18:45:29 +0100

    Create TeamsList component

---
+++ src/components/TeamsList/TeamsList.tsx
@@ -0,0 +1,21 @@
+import { Team } from "../../team/types";
+import TeamCard from "../TeamCard/TeamCard";
+import "./TeamsList.css";
+
+interface TeamsListProps {
+  teams: Team[];
+}
+
+const TeamsList: React.FC<TeamsListProps> = ({ teams }) => {
+  return (
+    <ul className="teams-list">
+      {teams.map((team) => (
+        <li key={team._id}>
+          <TeamCard team={team} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TeamsList;
