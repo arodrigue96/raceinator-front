64ee8128526eacc00933d21e0f02849dc29cacd7
Aniol Rodriguez <arodrigue96@gmail.com>
2024-12-06 20:04:35 +0100

    Create fetchTeams function and display TeamsList into TeamsPage component

--- src/team/pages/TeamsPage/TeamsPage.tsx
+++ src/team/pages/TeamsPage/TeamsPage.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import TeamsList from "../../../components/TeamsList/TeamsList";
+import TeamsClient from "../../client/TeamsClient";
+import { Team } from "../../types";
+
 const TeamsPage: React.FC = () => {
-  return <h1 className="page-title">Teams</h1>;
+  const [teams, setTeams] = useState<Team[]>([]);
+
+  useEffect(() => {
+    const fetchTeams = async () => {
+      try {
+        const teamsData = await new TeamsClient().getTeams();
+        setTeams(teamsData);
+      } catch (error) {
+        console.log("Error to fetch Teams:", error);
+      }
+    };
+
+    fetchTeams();
+  }, []);
+
+  return (
+    <>
+      <h1 className="page-title">Teams</h1>
+      <TeamsList teams={teams} />
+    </>
+  );
 };
 
 export default TeamsPage;
