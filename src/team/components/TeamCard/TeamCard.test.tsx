8b79df2da25775e3674c7b0c9beee38170e55fb8
Aniol Rodriguez <arodrigue96@gmail.com>
2024-12-06 23:16:02 +0100

    Add test cases for: TeamCard, TeamsList and TeamsPage

---
+++ src/team/components/TeamCard/TeamCard.test.tsx
@@ -0,0 +1,51 @@
+import { render, screen } from "@testing-library/react";
+import TeamCard from "./TeamCard";
+import { teamMock1 } from "../../../mocks/teamsMock";
+
+describe("Given the TeamCard component", () => {
+  describe("When it receives a team with the name 'Aniol's team'", () => {
+    test("Then it should show an image with 'The motorbikes of Aniol's team' alterantive text", () => {
+      const expectedAltImageText = /The motorbikes of Aniol's team/i;
+
+      render(<TeamCard team={teamMock1} />);
+
+      const teamCardAltImage = screen.getByAltText(expectedAltImageText);
+
+      expect(teamCardAltImage).toBeInTheDocument();
+    });
+
+    test("Then it should show 'Aniol's team' inside a heading", () => {
+      const expectedNameText = /Aniol's team/i;
+
+      render(<TeamCard team={teamMock1} />);
+
+      const teamName = screen.getByRole("heading", {
+        name: expectedNameText,
+      });
+
+      expect(teamName).toBeInTheDocument();
+    });
+
+    test("Then it should show 'Team riders' inside a heading", () => {
+      const expectedNameText = /Team riders/i;
+
+      render(<TeamCard team={teamMock1} />);
+
+      const teamRidersTitle = screen.getByRole("heading", {
+        name: expectedNameText,
+      });
+
+      expect(teamRidersTitle).toBeInTheDocument();
+    });
+
+    test("Then it should show 'Aniol Rodriguez and 'Erik Riquelme' texts", () => {
+      render(<TeamCard team={teamMock1} />);
+
+      const teamRider1 = screen.getByText(/Aniol Rodriguez/i);
+      const teamRider2 = screen.getByText(/Erik Riquelme/i);
+
+      expect(teamRider1).toBeInTheDocument();
+      expect(teamRider2).toBeInTheDocument();
+    });
+  });
+});
--- src/team/pages/TeamsPage/TeamsPage.test.tsx
+++ src/team/pages/TeamsPage/TeamsPage.test.tsx
@@ -14,5 +14,38 @@ describe("Given the TeamsPage component", () => {
 
       expect(pageTitle).toBeInTheDocument();
     });
+
+    test("Then it should show the images 'The motorbikes of Aniol's team' and 'The motorbikes of Mario's team' alternatives texts", async () => {
+      const expectedTeam1AltText = /The motorbikes of Aniol's team/i;
+      const expectedTeam2AltText = /The motorbikes of Mario's team/i;
+
+      render(<TeamsPage />);
+
+      const team1AlternativeText =
+        await screen.findByAltText(expectedTeam1AltText);
+      const team2AlternativeText =
+        await screen.findByAltText(expectedTeam2AltText);
+
+      expect(team1AlternativeText).toBeInTheDocument();
+      expect(team2AlternativeText).toBeInTheDocument();
+    });
+
+    test("Then it should show the team's name 'Aniol's team' and 'Mario's team' inside their headings", async () => {
+      const expectedTeamNameText1 = /Aniol's team/i;
+      const expectedTeamNameText2 = /Mario's team/i;
+
+      render(<TeamsPage />);
+
+      const teamName1 = await screen.findByRole("heading", {
+        name: expectedTeamNameText1,
+      });
+
+      const teamName2 = await screen.findByRole("heading", {
+        name: expectedTeamNameText2,
+      });
+
+      expect(teamName1).toBeInTheDocument();
+      expect(teamName2).toBeInTheDocument();
+    });
   });
 });
