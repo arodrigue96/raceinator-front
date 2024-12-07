6632e9c6268a01a4ca3b96c684c3136f842a5ac5
Aniol Rodriguez <arodrigue96@gmail.com>
2024-12-07 21:52:05 +0100

    Implement it into TeamsPage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,8 @@
 import { Outlet } from "react-router";
 import { Bounce, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "../Header/Header";
 import NavMenu from "../NavMenu/NavMenu";
-import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 const App: React.FC = () => {
