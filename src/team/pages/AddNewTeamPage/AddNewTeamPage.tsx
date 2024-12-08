0f79e90dd07f49dc50e33951fe96a040d1397f30
Aniol Rodriguez <arodrigue96@gmail.com>
2024-12-09 00:47:06 +0100

    Render it into AddNewTeamPage component

--- src/team/pages/AddNewTeamPage/AddNewTeamPage.tsx
+++ src/team/pages/AddNewTeamPage/AddNewTeamPage.tsx
@@ -1,5 +1,12 @@
+import TeamForm from "../../components/TeamForm/TeamForm";
+
 const AddNewTeamPage: React.FC = () => {
-  return <h1 className="page-title">Add new team</h1>;
+  return (
+    <>
+      <h1 className="page-title">Add new team</h1>
+      <TeamForm />
+    </>
+  );
 };
 
 export default AddNewTeamPage;
