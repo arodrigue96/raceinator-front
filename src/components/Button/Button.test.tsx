d12c11c2ec7c5d17c941ad1a66b68e7137acca31
Aniol Rodriguez <arodrigue96@gmail.com>
2024-12-10 22:54:27 +0100

    Add test case for Button component

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
 import Button from "./Button";
 
 describe("Given the Button component", () => {
@@ -13,6 +14,24 @@ describe("Given the Button component", () => {
       });
 
       expect(button).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's rendered and receives a 'linkTo'='/home' and 'Test' as text", () => {
+    test("Then it should show 'Test' link", () => {
+      const linkText = /Test/i;
+
+      render(
+        <MemoryRouter>
+          <Button text="Test" linkTo="/home" />
+        </MemoryRouter>,
+      );
+
+      const link = screen.getByRole("link", {
+        name: linkText,
+      });
+
+      expect(link).toBeInTheDocument();
     });
   });
 });
