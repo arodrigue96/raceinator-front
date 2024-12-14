3861e87c2e96b0a8fb2bf9eed577e19ab30bc297
Aniol Rodriguez <arodrigue96@gmail.com>
2024-12-14 16:19:34 +0100

    Add new http.post handler and a teamMock

--- src/team/mocks/teamsMock.ts
+++ src/team/mocks/teamsMock.ts
@@ -5,22 +5,34 @@ export const teamMock1: Team = {
   name: "Aniol's team",
   ridersNames: ["Aniol Rodriguez", "Erik Riquelme"],
   championshipTitles: 3,
-  imageUrl: "",
+  imageUrl: "https://aniol.com",
   altImageText: "The motorbikes of Aniol's team",
-  description: "",
+  description: "El equipo de Aniol",
   debutYear: 1996,
   isOfficialTeam: true,
 };
 
 export const teamMock2: Team = {
-  _id: "36b8f84d-df4e-4d49-b662-bcde71a8764f",
+  _id: "36b8f84d-df4e-4d49-b662-bcde71a8764g",
   name: "Mario's team",
   ridersNames: ["Mario Gonzalez", "Refactor Gonsalo"],
   championshipTitles: 0,
-  imageUrl: "",
+  imageUrl: "https://refactor.com",
   altImageText: "The motorbikes of Mario's team",
-  description: "",
+  description: "El equipo de Mario",
   debutYear: 2024,
+  isOfficialTeam: true,
+};
+
+export const teamMock3: Team = {
+  _id: "36b8f84d-df4e-4d49-b662-bcde71a8764h",
+  name: "Refactor's team",
+  ridersNames: ["Aniol Rodriguez", "Erik Riquelme"],
+  championshipTitles: 3,
+  imageUrl: "https://refacto.com",
+  altImageText: "The motorbikes of Aniol's team",
+  description: "El equipo de Aniol",
+  debutYear: 1996,
   isOfficialTeam: true,
 };
 
