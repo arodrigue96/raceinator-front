e54b9d1f8f96f3be781ef93b40055589f2866a5f
Aniol Rodriguez <arodrigue96@gmail.com>
2024-12-15 13:20:34 +0100

    Send team data to the API REST after the form submission

--- src/team/pages/AddNewTeamPage/AddNewTeamPage.tsx
+++ src/team/pages/AddNewTeamPage/AddNewTeamPage.tsx
@@ -1,10 +1,18 @@
 import TeamForm from "../../components/TeamForm/TeamForm";
+import useTeamForm from "../../hooks/useTeamForm";
+import { TeamWithoutId } from "../../types";
 
 const AddNewTeamPage: React.FC = () => {
+  const { createTeam } = useTeamForm();
+
+  const sendTeamDataToApiRest = async (teamData: TeamWithoutId) => {
+    await createTeam(teamData);
+  };
+
   return (
     <>
       <h1 className="page-title">Add new team</h1>
-      <TeamForm />
+      <TeamForm submitTeamData={sendTeamDataToApiRest} />
     </>
   );
 };
