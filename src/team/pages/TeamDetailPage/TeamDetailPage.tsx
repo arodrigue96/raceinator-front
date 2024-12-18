bc1b60199102c42397a54e32bfd4ff4e9a693030
Aniol Rodriguez <arodrigue96@gmail.com>
2024-12-18 13:26:43 +0100

    Create TeamDetailPage to render TeamDetail using teamId from url

---
+++ src/team/pages/TeamDetailPage/TeamDetailPage.tsx
@@ -0,0 +1,37 @@
+import { useCallback, useEffect } from "react";
+import { useParams } from "react-router";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks";
+import TeamDetail from "../../components/TeamDetail/TeamDetail";
+import { teamsClient } from "../../client/TeamsClient";
+import { loadTeam } from "../../slice";
+import { displayLoading, hideLoading } from "../../../uiSlice";
+import { loadingTeamError } from "../../toasts/errors/errors";
+
+const TeamDetailPage: React.FC = () => {
+  const { teamId } = useParams<{ teamId: string }>();
+  const team = useAppSelector((state) => state.teamsState.team);
+
+  const dispatch = useAppDispatch();
+
+  const fetchTeam = useCallback(async () => {
+    try {
+      dispatch(displayLoading());
+
+      const fetchTeam = await teamsClient.getTeamById(teamId as string);
+      dispatch(loadTeam(fetchTeam));
+
+      dispatch(hideLoading());
+    } catch {
+      dispatch(hideLoading());
+      loadingTeamError();
+    }
+  }, [dispatch, teamId]);
+
+  useEffect(() => {
+    fetchTeam();
+  }, [fetchTeam]);
+
+  return <TeamDetail team={team} />;
+};
+
+export default TeamDetailPage;
