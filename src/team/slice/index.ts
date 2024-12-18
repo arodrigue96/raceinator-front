c03726e5a5527ef26868fe30fc8132826a1b9708
Aniol Rodriguez <arodrigue96@gmail.com>
2024-12-18 13:00:24 +0100

    Add teamInitialState and loadTeam reducer in team store

--- src/team/slice/index.ts
+++ src/team/slice/index.ts
@@ -3,10 +3,22 @@ import { Team } from "../types";
 
 interface TeamsState {
   teams: Team[];
+  team: Team;
 }
 
 const teamsInitialState: TeamsState = {
   teams: [],
+  team: {
+    _id: "",
+    name: "",
+    ridersNames: ["", ""],
+    debutYear: 0,
+    isOfficialTeam: false,
+    championshipTitles: 0,
+    imageUrl: "",
+    altImageText: "",
+    description: "",
+  },
 };
 
 export const teamsSlice = createSlice({
@@ -19,8 +31,14 @@ export const teamsSlice = createSlice({
         teams: action.payload,
       };
     },
+    loadTeam: (state, action: PayloadAction<Team>) => {
+      return {
+        ...state,
+        team: action.payload,
+      };
+    },
   },
 });
 
-export const { loadTeams } = teamsSlice.actions;
+export const { loadTeams, loadTeam } = teamsSlice.actions;
 export default teamsSlice.reducer;
